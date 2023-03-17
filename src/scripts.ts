a464fa15db9d4bcb05132f5d17e03fa29ddaa309
edzusk <edzus.krisjanis@gmail.com>
2023-03-17 17:45:17 +0200

    first commit

---
+++ src/scripts.ts
@@ -0,0 +1,96 @@
+import sounds from "../utils/sounds";
+
+const ukulele = document.querySelector('.ukulele');
+const strumDOwn = ukulele.querySelector<HTMLButtonElement>('.downJs');
+const strumUp = ukulele.querySelector<HTMLButtonElement>('.upJs');
+const cChord = ukulele.querySelector('.cJs');
+const dChord = ukulele.querySelector('.dJs');
+const amChord = ukulele.querySelector('.amJs');
+const gChord = ukulele.querySelector('.gJs');
+const fChord = ukulele.querySelector('.fJs');
+const strings = ukulele.querySelectorAll('.stringJs')
+
+
+let currentSound: HTMLAudioElement = null;
+
+type Chords = 'clean' | 'am' | 'c' | 'd' | 'f' | 'g';
+
+let activeChord:Chords = 'clean'
+
+// Handle Active Chord changes on buttons
+const handleChord = (chordButton: Element, chord: Chords, key: string) => {
+  chordButton.addEventListener('mousedown', () => {
+    activeChord = chord
+    chordButton.classList.add('active')
+    strings.forEach((string, i) => string.classList.add(`${activeChord}${i+1}`))
+  })
+
+  document.addEventListener('keydown', (event) => {
+    if (event.code === key) {
+      activeChord = chord
+      chordButton.classList.add('active')
+      strings.forEach((string, i) => string.classList.add(`${activeChord}${i+1}`))
+    }
+  });
+
+  chordButton.addEventListener('mouseup', () => {
+    strings.forEach((string, i) => string.classList.remove(`${activeChord}${i+1}`))
+    activeChord = 'clean'
+    chordButton.classList.remove('active')
+  })
+
+  document.addEventListener('keyup', (event) => {
+    if (event.code === key) {
+      strings.forEach((string, i) => string.classList.remove(`${activeChord}${i+1}`))
+      activeChord = 'clean'
+      chordButton.classList.remove('active')
+    }
+  });
+}
+
+handleChord(cChord, 'c', 'KeyC');
+handleChord(dChord, 'd', 'KeyD');
+handleChord(amChord, 'am', 'KeyA');
+handleChord(gChord, 'g', 'KeyG');
+handleChord(fChord, 'f', 'KeyF');
+
+
+
+//Work with Strums
+type StrumDirection = 'up' | 'down'
+const strum = (dir: StrumDirection) => {
+  if (currentSound) {
+    currentSound.pause(); // stop the previous sound
+    currentSound.currentTime = 0; // reset to beginning
+  }
+  currentSound = sounds[dir][activeChord];
+  currentSound.currentTime = 0; // reset to beginning
+  currentSound.play();
+}
+
+strumDOwn.addEventListener('click', () => {
+  strum('down')
+});
+
+strumUp.addEventListener('click', () => {
+  strum('up')
+});
+
+//keyboard strums with arrows
+
+const keyboardStrum = (arrow: string, button: HTMLButtonElement) => {
+  document.addEventListener("keydown", (event) => {
+    if (event.code === arrow) {
+      button.click();
+      button.classList.add('active');
+    }
+  });
+  document.addEventListener("keyup", (event) => {
+    if (event.code === arrow) {
+      button.classList.remove('active');
+    }
+  });
+};
+
+keyboardStrum('ArrowUp', strumUp)
+keyboardStrum('ArrowDown', strumDOwn)
---
+++ utils/sounds.ts
@@ -0,0 +1,20 @@
+const sounds = {
+  up: {
+    clean: new Audio('./assets/sounds/clean-up.m4a'),
+    am: new Audio('./assets/sounds/Am-up.m4a'),
+    c: new Audio('./assets/sounds/C-up.m4a'),
+    d: new Audio('./assets/sounds/D-up.m4a'),
+    f: new Audio('./assets/sounds/F-up.m4a'),
+    g: new Audio('./assets/sounds/G-up.m4a'),
+  },
+  down: {
+    clean: new Audio('./assets/sounds/clean-down.m4a'),
+    am: new Audio('./assets/sounds/Am-down.m4a'),
+    c: new Audio('./assets/sounds/C-down.m4a'),
+    d: new Audio('./assets/sounds/D-down.m4a'),
+    f: new Audio('./assets/sounds/F-down.m4a'),
+    g: new Audio('./assets/sounds/G-down.m4a'),
+  },
+};
+
+export default sounds;
